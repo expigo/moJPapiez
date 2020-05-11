03fa0215b2e0317464a64bc5112261b8fbc73307
Kryspin Musiol <kryspin.musiol@gmail.com>
2020-05-11 23:47:48 +0200

    Add basic security measures

--- back/src/app.js
+++ back/src/app.js
@@ -4,6 +4,12 @@ require('dotenv').config({
 
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitze = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
 const config = require('./config/index')
 const AppError = require('./utils/AppError')
 const monumentRouter = require('./resources/monument/monument.router')
@@ -12,14 +18,34 @@ const errorFrontController = require('./utils/errorFrontController')
 
 const app = express()
 
+app.use(helmet())
+
 app.disable('x-powered-by')
 
 if (config.env == 'development') {
   app.use(morgan('dev'))
 }
 
-app.use(express.json())
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:
+    'The threshold of max request from this IP has been reached. Please try again later',
+})
 
+app.use(express.json({limit: '10kb'}))
+
+app.use(mongoSanitze())
+
+app.use(xss())
+
+app.use(
+  hpp({
+    whitelist: ['ratingsAverage', 'ratingsQuantity', 'category'],
+  })
+)
+
+app.use('/api', limiter)
 app.use('/api/v1/monuments', monumentRouter)
 app.use('/api/v1/users', userRouter)
 
